8ca07aaaebefa8c5de638a011f64993d17c0c395
Pilar Arriagada <arriagada.pilar@gmail.com>
2026-08-12 00:42:00 +0200

    Finalisation des pages accueil et galerie responsive

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,6 +1,10 @@
 import Route from "./Route.js";
-//Définir ici vos routes
+
+// Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
-//Le titre s'affiche comme ceci : Route.titre - websitename
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+];
+
+// Le titre s'affiche comme ceci : Route.titre - websiteName
 export const websiteName = "Quai Antique";
