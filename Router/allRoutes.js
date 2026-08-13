7fbafe14c7cc4a5fbf0907a890b9bf3780071f12
Pilar Arriagada <arriagada.pilar@gmail.com>
2026-08-13 19:06:12 +0200

    feat: ajout de la gestion du compte, de la galerie et des réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,12 +1,16 @@
 import Route from "./Route.js";
 
-// Définir ici mes routes
+// Mes routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "/pages/signin.html"),
     new Route("/signup","Inscription", "/pages/signup.html"),
+    new Route("/account", "Mon compte", "/pages/account.html"),
+    new Route("/editPassword", "Changer mon mot de passe", "/pages/editPassword.html"),
+    new Route("/reservations", "Mes réservations", "/pages/reservations.html"),
+    new Route("/reserver", "Ma réservation", "/pages/reserver.html"),
 ];
 
-// Le titre s'affiche comme ceci : Route.titre - websiteName
+// Exemple : "Mon compte - Quai Antique"
 export const websiteName = "Quai Antique";
